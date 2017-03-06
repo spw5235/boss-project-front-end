7aa809ea6cce3d67e86e6ff0611c604c40bb4004
Stephen Wisner <spwisner@gmail.com>
2017-03-05 21:12:36 -0500

    Heroku troubleshooting

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     // production: 'http://localhost:4741',
-    production: 'https://stud-tracker.herokuapp.com',
+    production: 'https://boss-api.herokuapp.com',
   },
 };
 
